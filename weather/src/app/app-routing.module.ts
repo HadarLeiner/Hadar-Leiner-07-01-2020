651f8df6e7d759c95f9b1d251c17b1db7bfec03b
HadarLeiner <crocodil770@gmail.com>
2020-01-10 16:17:04 +0200

    change 10.1

--- weather/src/app/app-routing.module.ts
+++ weather/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'favorites', component: FavoritesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ weather/src/app/components/main/main.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ServiceWeatherService } from '../../service/service-weather.service'
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+  Forecasts: any;
+  Current: any;
+  location: String;
+  locationKey = "215854";
+  locationsFavorites;
+  citiesArr;
+  constructor(private _WeatherService: ServiceWeatherService) { }
+
+  ngOnInit() {
+    this._WeatherService.currentFavoritesArr.subscribe(location =>
+      this.location = location)
+    this.location = "Tel Aviv";
+
+    this._WeatherService.getCurrent(this.locationKey).subscribe((d) => {
+      this.Current = d[0].Temperature.Imperial.Value;
+    });;
+
+    this._WeatherService.getForecasts(this.locationKey).subscribe((d) => {
+      this.Forecasts = Object.values(d)[1];
+    });;
+  }
+
+  auto() {
+    this._WeatherService.getAuto(this.location).subscribe((d) => {
+      console.log(d);
+      for (let name in d) {
+        this.citiesArr.push(name)
+      }
+      this.Current = d[0];
+      this.locationKey = this.Current.Key;
+    });
+  }
+  getWeather() {
+    this._WeatherService.getCurrent(this.locationKey).subscribe((d) => {
+      this.Current = d[0].Temperature.Imperial.Value;
+      this.location = this.location;//i'm not sure if i need this line
+    });
+  }
+  favorites() {
+    this.locationsFavorites.push(this.location);
+    this._WeatherService.changeFavoritesArr(this.locationsFavorites)
+  }
+}
